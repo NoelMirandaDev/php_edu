7f56ca6ef22e8480a56b446543494390e1838d7e
NoelMirandaDev <nymiranda1999@gmail.com>
2025-05-22 15:28:36 -0500

    Initial commit of php hello world exercise with a simple celsius to fahrenheit conversion

---
+++ helloWorld.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "Hello World!\n";
+
+$name = 'Noel';
+
+echo 'Welcome to php, ' . $name . "!\n\n";
+
+echo "This is a temperature converter exercise from Celsius to Fahrenheit.\n";
+$celsius = 35;
+$fahrenheit = ($celsius * 9/5) + 32;
+
+echo "{$celsius}C is {$fahrenheit}F";
