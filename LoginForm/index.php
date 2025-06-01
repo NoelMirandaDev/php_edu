65cd9fc8800fc42685562cbd99037e47fcee81cf
NoelMirandaDev <nymiranda1999@gmail.com>
2025-06-01 16:20:14 -0500

    Login Form Version 3 with all the php logic being handled before the rendering of the html. Error message for invalid csrf token was implemented and user input is sanitized to avoid XSS attacks.

--- LoginForm/index.php
+++ LoginForm/index.php
@@ -1,12 +1,27 @@
 <?php 
 declare (strict_types=1);
-    session_start();
+session_start();
+require_once 'CSRF.php';
 
-    require_once 'CSRF.php';
+$error = '';
+$success = false;
+$sanitizedInputs = [];
 
-    if (empty($_SESSION['csrf_token'])) {
+// If csrf token is empty it generates a new one
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = CSRF::generateToken();
+   }
+
+// Handles POST request method logic
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (CSRF::validToken($_SESSION['csrf_token'], $_POST['csrf_token'])) {
         $_SESSION['csrf_token'] = CSRF::generateToken();
+        $success = true;
+        $sanitizedInputs = array_map('htmlspecialchars', $_POST);
+    } else {
+        $error = 'Was unable to successfully log in. Please try again.';
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,43 +35,34 @@ declare (strict_types=1);
     crossorigin="anonymous">
 </head>
 <body>
-    <?php if ($_SERVER['REQUEST_METHOD'] == 'GET'): ?>
     <nav class="navbar navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/loginform/">Updated Login Form</a>
+            <a class="navbar-brand" href="/loginform/">Login Form v3</a>
         </div>
     </nav>
-    <form method="POST" action="/loginform/" class="d-flex justify-content-center mt-5 container">
-        <div class="d-flex flex-column border p-4 rounded shadow-sm bg-light container" style="max-width: 600px;">
-            <input type="hidden" name="csrf_token" value="<?=$_SESSION['csrf_token']?>"/>
+    <?php if (false === $success): ?>
+        <?php if ($error): ?>
+            <p class="text-danger bg-danger-subtle w-25 text-center m-5"><?=$error?></p>
+        <?php endif; ?>
+        <form method="POST" action="/loginform/" class="d-flex justify-content-center mt-5 container">
+            <div class="d-flex flex-column border p-4 rounded shadow-sm bg-light container" style="max-width: 600px;">
+                <input type="hidden" name="csrf_token" value="<?=$_SESSION['csrf_token']?>"/>
 
-            <div class="p-2">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" id="username" name="username" class="form-control"/>
+                <div class="p-2">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" required />
+                </div>
+
+                <div class="p-2 mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" required />
+                </div>
+                <button type="submit" class="btn btn-primary w-25 m-auto">Login</button>
             </div>
-
-            <div class="p-2 mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" id="password" name="password" class="form-control"/>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </div>
-    </form>
-<?php endif; ?>
-
-<?php if($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
-        <?php if(CSRF::validToken($_SESSION['csrf_token'], $_POST['csrf_token'])): ?>
-            <?php $_SESSION['csrf_token'] = CSRF::generateToken(); ?>
-        <?=var_dump($_POST);?>
-        <br/>
-        <?= var_dump($_SESSION['csrf_token']) ?>
-        <br/><br/>
-        <a href="/loginform/">back to form</a>
+        </form>
     <?php else: ?>
-        <p class="text-danger bg-danger-subtle w-auto text-center mt-5">Invalid CSRF token</p>
-        <br/><br/>
-        <a href="/loginform/" class="d-flex flex-column text-center">back to form</a>
+        <p class="m-5">Hello, <?=$sanitizedInputs['username'] ? $sanitizedInputs['username'] : 'stranger'?>!</p>
+        <a href="/loginform/" class="d-flex flex-column text-center mt-3">Back to form</a>
     <?php endif; ?>
-<?php endif; ?>
 </body>
 </html>
