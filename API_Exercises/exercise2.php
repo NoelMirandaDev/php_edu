67007249716f58525e229150c700c6f060f6fe0f
NoelMirandaDev <nymiranda1999@gmail.com>
2025-06-04 16:23:28 -0500

    Corrected explanation in the php comments

--- API_Exercises/exercise2.php
+++ API_Exercises/exercise2.php
@@ -1,14 +1,14 @@
 <?php 
 
-// Decoding a response (In JSON) from an API call.
+// Assign response (in JSON format) to variable from an API call.
 $response = file_get_contents('https://randomuser.me/api');
 
-// Turning the JSON response to a PHP object (stdClass) utilizing json_decode function (default behavior)
+// Decodes the JSON response to a PHP object (stdClass) utilizing json_decode function (default behavior)
 $data = json_decode($response);
 var_dump($data->results[0]->gender);
 echo "\n";
 
-// Other option is to turn the response to an associative array for easier data retrievable
+// Other option is to turn the JSON response to an associative array for easier data retrievable
 $data2 = json_decode($response, true);
 var_dump($data2['results'][0]['gender']);
 
