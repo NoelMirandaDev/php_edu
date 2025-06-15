3d88ea162b16af1e239a6946219b077e558de647
NoelMirandaDev <nymiranda1999@gmail.com>
2025-06-15 09:48:54 -0500

    Simple task list project to familiarize oneself with laravel. This commit focused mainly on interacting with the routes.

---
+++ laravel12/task-list/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Main Page';
+})->name('home');
+
+Route::get('/xxx', function () {
+    return 'Hello World';
+}) -> name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', fn($name) =>
+    "Hello {$name}!"
+)->name('greeting');
+
+Route::get('/not-found', function () {
+    return 'Sorry, this page does not exist';
+})->name('404');
+
+Route::fallback(fn() => redirect()->route('404'));
