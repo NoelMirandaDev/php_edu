6db211057a38b34a8c1e2a744a7e968ca81609e5
NoelMirandaDev <nymiranda1999@gmail.com>
2025-06-15 22:33:20 -0500

    fixed a typo

--- laravel12/task-list/resources/views/index.blade.php
+++ laravel12/task-list/resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @if (!empty($name))
 <div>Hello {{ $name }}, I am a blade template!</div>
 @else
-<div>stranger, I am a blade template!</div>
+<div>Hello stranger, I am a blade template!</div>
 @endif
