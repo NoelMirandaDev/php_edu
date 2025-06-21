3e582f6ec95c2b62258dcb210122202cb091efab
NoelMirandaDev <nymiranda1999@gmail.com>
2025-06-20 22:09:05 -0500

    EDU feat: Post route, form blade template, and CSRF protection implemented.

--- laravel12/task-list/routes/web.php
+++ laravel12/task-list/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use App\Models\Task as TaskModel;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -14,12 +15,19 @@ Route::get('/tasks', function ()  {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')
+->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show',[
             'task' => TaskModel::findOrFail($id),
           ]
 );
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 // Route::get('/xxx', function () {
 //     return 'Hello World';
