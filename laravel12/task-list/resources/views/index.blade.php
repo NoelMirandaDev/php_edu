4c23922f10c72c8316f70d48e3c158becf9c3977
NoelMirandaDev <nymiranda1999@gmail.com>
2025-07-05 17:37:41 -0500

    EDU feat: added some styling with tailwind css

--- laravel12/task-list/resources/views/index.blade.php
+++ laravel12/task-list/resources/views/index.blade.php
@@ -12,20 +12,22 @@
 
 @section('content')
 
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+        class="font-medium text-grey-700 underline decoration-pink-500">Create a new task</a>
+    </nav>
+
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task]) }}"
+                @class(['line-through' => $task->completed])>{{ $task->title }}</a>
         </div>
     @empty
     <p>There are no Tasks!</p>
     @endforelse
 
-    <div>
-        <a href="{{ route('tasks.create') }}">Create a new task</a>
-    </div>
-
     @if($tasks->count())
-        <nav>
+        <nav class="mt-4">
             {{ $tasks->links() }}
         </nav>
     @endif
