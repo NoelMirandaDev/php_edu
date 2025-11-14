446cb724f1184d53c00ecd38cdecab98c1ce501e
NoelMirandaDev <nymiranda1999@gmail.com>
2025-11-13 20:48:50 -0600

    EDU feat: Added more local query scopes to Book Model

--- laravel12/book-review/app/Models/Book.php
+++ laravel12/book-review/app/Models/Book.php
@@ -19,4 +19,28 @@ class Book extends Model
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeUnpopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'asc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeLowestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'asc');
+    }
 }
