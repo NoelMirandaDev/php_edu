27c23d4715cb2eb291040e67375d2c9d2da16d2c
NoelMirandaDev <nymiranda1999@gmail.com>
2026-01-02 22:03:01 -0600

    EDU feat: implement livewire validation

--- laravel12/livewire-poll/resources/views/livewire/create-poll.blade.php
+++ laravel12/livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -3,9 +3,18 @@
         <label>Poll Title</label>
         <input type="text" wire:model.live="title" />
 
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mtb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
+
+         @error('options')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div>
             @foreach ($options as $index => $option)
                 <div class="mt-4">
@@ -15,6 +24,9 @@
                         <input type="text" wire:model.live="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
