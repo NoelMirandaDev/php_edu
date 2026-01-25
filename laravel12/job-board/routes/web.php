a3434d54ea784e25f5f09050c6345aa3e353b9c7
NoelMirandaDev <nymiranda1999@gmail.com>
2026-01-24 18:56:02 -0600

    EDU feat: implemented authentication controller

--- laravel12/job-board/routes/web.php
+++ laravel12/job-board/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::get('/', fn() => to_route('jobs.index'));
+Route::get('login', fn() => to_route('auth.create'))
+    ->name('login');
 
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
+
+Route::resource('auth', AuthController::class)
+    ->only(['create', 'store']);
